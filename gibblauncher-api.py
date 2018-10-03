3ccfd05fd49e04bfc3c9fd58cb461753208b9641
Ronyell Henrique <ronyellhenrique@gmail.com>
2018-10-03 20:59:37 -0300

    #1 - Adding random int

--- gibblauncher-api.py
+++ gibblauncher-api.py
@@ -1,7 +1,54 @@
 from flask import Flask, jsonify, request
 import time
+import random
+
 
 app = Flask(__name__)
+
+response_bounces = {
+        'bounces': [
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+            {
+              'x': random.randint(-100,100),
+              'y': random.randint(-100,100),
+            },
+        ]
+    }
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -10,12 +57,37 @@ def hello():
 
 @app.route('/', methods=['POST'])
 def set_players():
+    """
+    {
+        'id': 12,
+        'position': -1,
+        'shots': [
+            50, 12, 33, 77
+        ]
+    }
+    """
     # Sleep to simulate image processing
     time.sleep(20)
 
     # Print request to confirm post data
     print(request.json)
-    return jsonify({'data': "Post Confirm!"})
+
+    # Save in file
+    """
+    {
+        'bounces': [
+            {
+              'x': 50,
+              'y': 30,
+            },
+            {
+              'x': -1,
+              'y': -1,
+            },...
+        ]
+    }
+    """
+    return jsonify(response_bounces)
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
