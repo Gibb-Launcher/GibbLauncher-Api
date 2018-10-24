e9594fad0d77754e533a7adb0ee7e5ec14662dec
Thiago Nogueira <thiagonf10@gmail.com>
2018-10-24 00:41:32 -0200

    Adding control ip acess API

--- gibblauncher-api.py
+++ gibblauncher-api.py
@@ -1,11 +1,49 @@
 from flask import Flask, jsonify, request
 import time
 import random
+import os
 
-
+IP_MUTEX = None
 app = Flask(__name__)
 
-response_bounces = {
+@app.route('/', methods=['GET'])
+def hello():
+  return jsonify({'data': "Get Confirm!"})
+
+
+@app.route('/', methods=['POST'])
+def start_request():
+  global IP_MUTEX
+  requestIP = str(request.get_json()['ip'])
+
+  if IP_MUTEX == None or IP_MUTEX == requestIP:
+    print("IP_MUTEX = " + str(IP_MUTEX))
+    IP_MUTEX = requestIP
+    response = set_players()
+  else :
+    response = checkIp(requestIP)
+
+  return jsonify(response)
+
+def checkIp(requestIP):
+  global IP_MUTEX
+  if isAvailable() != 0 :
+    print("verificou o ip")
+    responseJson = set_players()
+    IP_MUTEX = requestIP 
+  else :
+    responseJson = {'data': "Ocupado!"}
+    print("Ocupado...")
+
+  return responseJson
+
+def isAvailable() :
+  response = os.system("ping -c 1 " + IP_MUTEX)
+  print("Response ping: " + str(response))
+  return response
+
+def set_players():
+    response_bounces = {
         'bounces': [
             {
               'x': random.randint(-100,100),
@@ -50,13 +88,6 @@ response_bounces = {
         ]
     }
 
-@app.route('/', methods=['GET'])
-def hello():
-    return jsonify({'data': "Get Confirm!"})
-
-
-@app.route('/', methods=['POST'])
-def set_players():
     """
     {
         'id': 12,
@@ -67,7 +98,7 @@ def set_players():
     }
     """
     # Sleep to simulate image processing
-    time.sleep(20)
+    time.sleep(2)
 
     # Print request to confirm post data
     print(request.json)
@@ -87,7 +118,7 @@ def set_players():
         ]
     }
     """
-    return jsonify(response_bounces)
+    return response_bounces
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
